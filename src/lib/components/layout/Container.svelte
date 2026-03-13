144cb4fca338620c6442048c70232e4ea59f1ecf
luisfferreiraDev <luisfferreira.dev@gmail.com>
2026-03-13 23:41:26 +0000

    Large screen container fix

--- src/lib/components/layout/Container.svelte
+++ src/lib/components/layout/Container.svelte
@@ -9,18 +9,18 @@
 <div
 	class="fixed top-0 right-0 left-11 z-30 box-content flex h-22 flex-col border-b border-medium-gray bg-light-gray"
 >
-	<div class="relative h-full w-full">
+	<div class="relative h-full w-full px-2">
 		<a href={resolve(paths.home.home())} class="flex h-11 items-center gap-2 pl-6 font-bold">
 			<ChevronLeftIcon class=" text-dark-gray" />
 			<p>Tickets</p>
 		</a>
 		<div
-			class="absolute right-2 bottom-0 left-2 flex h-11 items-center rounded-t-lg border-x border-t border-medium-gray bg-white px-8"
+			class="mx-auto flex h-11 max-w-400 items-center rounded-t-lg border-x border-t border-medium-gray bg-white px-8"
 		>
 			{@render header()}
 		</div>
 	</div>
 </div>
-<div class="relative z-20 rounded-lg border border-medium-gray bg-white pt-11">
+<div class="relative z-20 mx-auto max-w-400 rounded-lg border border-medium-gray bg-white pt-11">
 	{@render children()}
 </div>
